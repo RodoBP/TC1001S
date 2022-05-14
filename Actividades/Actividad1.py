a63f435c67cb8234895781ca3412754bc4e23a18
Rodolfo Bojorquez <rodobp97@gmail.com>
2022-05-13 18:15:00 -0700

    [feat] Se modifico Actividad1.py

--- Actividades/Actividad1.py
+++ Actividades/Actividad1.py
@@ -1,31 +1,17 @@
-"""Paint, for drawing shapes.
-
-Exercises
-
-1. Add a color.
-2. Complete circle.
-3. Complete rectangle.
-4. Complete triangle.
-5. Add width parameter.
-"""
-
-from re import X
 from turtle import *
-from typing import Counter
-
 from freegames import vector
+import turtle
 
 
 def line(start, end):
-    """Draw line from start to end."""
+    "Draw line from start to end."
     up()
     goto(start.x, start.y)
     down()
     goto(end.x, end.y)
-
 
 def square(start, end):
-    """Draw square from start to end."""
+    "Draw square from start to end."
     up()
     goto(start.x, start.y)
     down()
@@ -37,52 +23,57 @@ def square(start, end):
 
     end_fill()
 
-
+# Dibujar Circulo
 def circle(start, end):
-    """Draw circle from start to end."""
-
+    "Draw circle from start to end."
+    pass  # TODO
     up()
     goto(start.x, start.y)
     down()
-    goto(end.x, end.y)
-    begin_fill()
-    
-    x = end.x - start.x
-
-    circle(x)
-
-    end_fill()
 
 
+    t = turtle.Turtle()
+    r = 30
+    t.begin_fill()
+    t.circle(r)
+
+    t.end_fill()
+
+#Dibujar rectangulo
 def rectangle(start, end):
-    """Draw rectangle from start to end."""
+    "Draw rectangle from start to end."
+    pass  # TODO
     up()
     goto(start.x, start.y)
     down()
     begin_fill()
 
-    for count in range(4):
+    for count in range (2):
         forward(end.x - start.x)
+        left(90)
+        forward(end.x/2 - start.x/2)
         left(90)
 
     end_fill()
 
+# Dibujar Triangulo
 def triangle(start, end):
-    """Draw triangle from start to end."""
+    "Draw triangle from start to end."
+    pass  # TODO
+
     up()
     goto(start.x, start.y)
     down()
     begin_fill()
 
-    for count in range(3):
+    for count in range (2):
         forward(end.x - start.x)
-        left(120)
+        left(90)
 
     end_fill()
-
 
 def tap(x, y):
-    """Store starting point or draw shape."""
+    "Store starting point or draw shape."
     start = state['start']
 
     if start is None:
@@ -93,11 +84,9 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
-
 def store(key, value):
-    """Store value in state at key."""
+    "Store value in state at key."
     state[key] = value
-
 
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
@@ -109,6 +98,8 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+# Color Nuevo es naranja
+onkey(lambda: color('orange'), 'O')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
